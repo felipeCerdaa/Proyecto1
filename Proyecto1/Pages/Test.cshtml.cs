b1177d07e0230d4384cb3a2f609ec51b620a0975
felipeCerdaa <fcerdaarancibia@gmail.com>
2022-09-02 19:02:50 -0400

    Commit

--- Proyecto1/Pages/Test.cshtml.cs
+++ Proyecto1/Pages/Test.cshtml.cs
@@ -5,8 +5,18 @@ namespace Proyecto1.Pages
 {
     public class TestModel : PageModel
     {
+        [BindProperty]
+        public int? numero1 { get; set; }
+        [BindProperty]
+        public int? numero2 { get; set; }
         public void OnGet()
         {
+            //this.numero1 = 123;
+            //this.numero2 = 321;
+        }
+        public void OnPost() 
+        {
+        
         }
     }
 }
