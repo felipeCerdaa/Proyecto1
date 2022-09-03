d597e4527d38ac4d615a6cde3845f7fdaca3b8b0
felipeCerdaa <fcerdaarancibia@gmail.com>
2022-09-03 03:44:29 -0400

    Commint

---
+++ Proyecto1/Pages/Categorias.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto1.Pages
+{
+    public class CategoriasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
